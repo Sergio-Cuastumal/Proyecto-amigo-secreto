d83ab6dd7ee60ee5bf2c76f6a87e2f8409ede769
Sergio-Cuastumal <n.andrescuastumal@gmail.com>
2025-08-06 20:47:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,28 +4,34 @@ let listaAmigos = [ ];
 
 function agregarAmigo(){
     
-    //ingresar nombre de amigo
-    let amigo = document.getElementById('amigo').value;
-    //console.log(amigo);
-    //evaluar dato valido
-    if(amigo == ''){
-        alert('Ingrese un nombre válido por favor.');
+    if(listaAmigos == ['']){
+        document.getElementById('sorteo').setAttribute('disabled',true);
     }else{
-        listaAmigos.push(amigo);
+        document.getElementById('sorteo').removeAttribute('disabled');
+        //ingresar nombre de amigo
+        let amigo = document.getElementById('amigo').value;
+        console.log(amigo);
+        //evaluar dato valido
+        if(amigo == '' || amigo == parseInt(amigo) ){
+            alert('Ingrese un nombre válido por favor.');
+        }else{
+            listaAmigos.push(amigo);
+        }
+        //console.log(typeof(amigo));
+        //adicionar a la lista de amigos
+        console.log(listaAmigos);
+        console.log(typeof(listaAmigos.length));//numero
+        //borrar texto luego de enviar nombre
+        document.querySelector('#amigo').value='';
+        mostrarLista();
     }
-    //console.log(typeof(amigo));
-    //adicionar a la lista de amigos
-    //console.log(listaAmigos);
-    //console.log(typeof(listaAmigos.length));//numero
-    //borrar texto luego de enviar nombre
-    document.querySelector('#amigo').value='';
-    mostrarLista();
+    
     
 }
 function sortearAmigo(){
     //designar posición del array
     let posicion = posicionAleatoria(listaAmigos.length);
-    //console.log(posicion);
+    console.log(posicion);
     //alert(`Tu amigo secreto es: ${listaAmigos[posicion]}`);
     document.getElementById('resultado').innerHTML= "Tu amigo secreto es "+listaAmigos[posicion];
     
@@ -40,16 +46,14 @@ function mostrarLista(){
     //variable para la lista en el HTML
     let listaHTML = document.getElementById('listaAmigos');
     listaHTML.innerHTML = "";//limpiar lista
-    //console.log(listaHTML);
+    console.log(listaHTML);
     for (let i = 0; i < listaAmigos.length; i++) {
         //almacenar un elemento 'li' en itemLista
         let itemLista = document.createElement('li');
-        //console.log(typeof(itemLista));
+        console.log(typeof(itemLista));
         //asignar el contenido a la posicion i
         itemLista.textContent = listaAmigos[i];
         //agregar el elemento li
         listaHTML.appendChild(itemLista);
     }
 }
-
-
