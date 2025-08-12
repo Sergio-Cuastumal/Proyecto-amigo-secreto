8552ed535fa72724fda81c0c72f01ef8c3eee221
Sergio Cuastumal <n.andrescuastumal@gmail.com>
2025-08-12 15:54:57 -0500

    Agrega restricciones de datos

--- app.js
+++ app.js
@@ -1,50 +1,48 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let listaAmigos = [ ];
+//variable para la lista en el HTML
+let listaHTML = document.getElementById('listaAmigos'); 
+    
 
 function agregarAmigo(){
-    
-    if(listaAmigos == ['']){
-        document.getElementById('sorteo').setAttribute('disabled',true);
+    //document.getElementById('sorteo').removeAttribute('disabled');
+    //ingresar nombre de amigo
+    let amigo = document.getElementById('amigo').value;
+    console.log(amigo);
+    //evaluar dato valido
+    if(amigo == '' || amigo == parseInt(amigo) ||listaHTML=='' ){
+        alert('Ingrese un nombre válido por favor.');
     }else{
-        document.getElementById('sorteo').removeAttribute('disabled');
-        //ingresar nombre de amigo
-        let amigo = document.getElementById('amigo').value;
-        console.log(amigo);
-        //evaluar dato valido
-        if(amigo == '' || amigo == parseInt(amigo) ){
-            alert('Ingrese un nombre válido por favor.');
-        }else{
-            listaAmigos.push(amigo);
-        }
-        //console.log(typeof(amigo));
-        //adicionar a la lista de amigos
-        console.log(listaAmigos);
-        console.log(typeof(listaAmigos.length));//numero
-        //borrar texto luego de enviar nombre
-        document.querySelector('#amigo').value='';
-        mostrarLista();
+        listaAmigos.push(amigo);
     }
-    
-    
-}
-function sortearAmigo(){
-    //designar posición del array
-    let posicion = posicionAleatoria(listaAmigos.length);
-    console.log(posicion);
-    //alert(`Tu amigo secreto es: ${listaAmigos[posicion]}`);
-    document.getElementById('resultado').innerHTML= "Tu amigo secreto es "+listaAmigos[posicion];
-    
+    //console.log(typeof(amigo));
+    //adicionar a la lista de amigos
+    console.log(listaAmigos);
+    console.log(typeof(listaAmigos.length));//numero
+    //borrar texto luego de enviar nombre
+    document.querySelector('#amigo').value='';
+    mostrarLista();
 }
 
+function sortearAmigo(){
+    if(listaAmigos.length!=0){
+        //designar posición del array
+        let posicion = posicionAleatoria(listaAmigos.length);
+        console.log(posicion);
+        //alert(`Tu amigo secreto es: ${listaAmigos[posicion]}`);
+        document.getElementById('resultado').innerHTML= "Tu amigo secreto es "+listaAmigos[posicion];
+        listaHTML.innerHTML='';
+    }else{
+        alert('La lista está vacía. Ingrese un nombre válido');
+    }
+}
 //generar numero para posicion aleatoria
 function posicionAleatoria(limSup){
     return Math.floor(Math.random()*limSup);
 }
-
 function mostrarLista(){
-    //variable para la lista en el HTML
-    let listaHTML = document.getElementById('listaAmigos');
+    
     listaHTML.innerHTML = "";//limpiar lista
     console.log(listaHTML);
     for (let i = 0; i < listaAmigos.length; i++) {
